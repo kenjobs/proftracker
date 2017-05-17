d901d6c16efe5a37d10080a68ca8c38f7717b00c
Ken Jobs <kenjobs@github.com>
2017-05-17 13:36:54 +0800

    Prepare handlers

--- Sources/App/Controllers/ProfessorsController.swift
+++ Sources/App/Controllers/ProfessorsController.swift
@@ -8,22 +8,33 @@ final class ProfessorsController {
         basic.post(handler: addProfessor)
         basic.get(handler: index)
         basic.delete(Professor.self, handler: delete)
-        basic.get(Professor.self, handler: showProfessor)
+        basic.get(Professor.self, handler: show)
+        
+        
+        basic.post(Professor.self, "createSchedule", handler: createSchedule)
+        basic.get(Professor.self, "schedules", handler: showSchedules)
     }
+    
+    func createSchedule(request: Request, professor: Professor) throws -> ResponseRepresentable {
+        var schedule = try request.schedule()
+        try schedule.save()
+        
+        return schedule
+    }
+    
+    func showSchedules(request: Request, professor: Professor) throws -> ResponseRepresentable {
+        let schedules = try professor.schedules()
+        
+        return try JSON(node: schedules.makeNode())
+    }
+
     
     func index(request: Request) throws -> ResponseRepresentable {
         print("Track this one")
         return try JSON(node: Professor.all().makeNode())
     }
     
-    
-    func showProfessor(request: Request, professor: Professor) throws -> ResponseRepresentable {
-        return professor
-    }
-    
     func create(request: Request) throws -> ResponseRepresentable {
-        
-        print("CALLING IN!")
         var professor = try request.professor()
         try professor.save()
         return professor
@@ -82,5 +93,13 @@ extension Request {
         }
         
         return try Professor(node: json)
+    }
+    
+    func schedule() throws -> Schedule {
+        guard let json = json else {
+            throw Abort.badRequest
+        }
+        
+        return try Schedule(node: json)
     }
 }
